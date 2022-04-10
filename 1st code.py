9a55eda95d5b72c02d2e99fba0b02d79452c872b
yaminidubey <yaminidubey1996@gmail.com>
2022-04-11 01:05:55 +0530

    2nd

--- 1st code.py
+++ 1st code.py
@@ -1,9 +1,8 @@
-s = "India is a country of agriculture and farmers are the pillar"
+S = "India is a country of agriculture and farmers are the pillar"
 d = {}
 for a in s:
     if a in d:
         d[a] = d[a]+1
     else:
         d[a]=1
-
 print(d)
